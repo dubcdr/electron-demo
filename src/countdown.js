3178efd30fa0056c0219faca7c94523f91e50ace
dubcdr <devinriley@gmai.com>
2017-07-28 22:16:44 -0500

    countdown timer

---
+++ src/countdown.js
@@ -0,0 +1,11 @@
+module.exports = function countdown(tick) {
+  let count = 10;
+
+  let timer = setInterval(() => {
+    tick(count--);
+    console.log(count);
+    if (count === 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
